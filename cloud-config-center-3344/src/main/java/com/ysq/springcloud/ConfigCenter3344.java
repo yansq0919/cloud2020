463193a07f832e63941ea4aa07265a7ad75bbb46
yansiqing <2457008948@qq.com>
2022-10-27 16:45:28 +0800

    cloud2020-commit

---
+++ cloud-config-center-3344/src/main/java/com/ysq/springcloud/ConfigCenter3344.java
@@ -0,0 +1,20 @@
+package com.ysq.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * 功能描述
+ *
+ * @author 闫思晴
+ * @date 2022/10/27  15:37
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenter3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenter3344.class,args);
+    }
+}
