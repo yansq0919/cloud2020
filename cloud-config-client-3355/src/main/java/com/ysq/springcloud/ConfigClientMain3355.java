f38dc0f780d42a6441b486a9d4abda1867cd7135
yansiqing <2457008948@qq.com>
2022-10-27 19:31:49 +0800

    cloud2020-commit

---
+++ cloud-config-client-3355/src/main/java/com/ysq/springcloud/ConfigClientMain3355.java
@@ -0,0 +1,19 @@
+package com.ysq.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * 功能描述
+ *
+ * @author 闫思晴
+ * @date 2022/10/27  17:11
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3355 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3355.class,args);
+    }
+}
---
+++ cloud-config-client-3355/src/main/java/com/ysq/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,21 @@
+package com.ysq.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 功能描述
+ *
+ * @author 闫思晴
+ * @date 2022/10/27  17:12
+ */
+@RestController
+public class ConfigClientController {
+    @Value("${config.info}")
+    private String configInfo;
+    @GetMapping("/configInfo")
+    public String getConfigInfo(){
+        return configInfo;
+    }
+}
