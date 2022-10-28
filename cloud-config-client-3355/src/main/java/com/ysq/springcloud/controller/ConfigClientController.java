a448cdf3f8bd872ed7d1118f77df57fe4965f37d
yansiqing <2457008948@qq.com>
2022-10-28 16:05:28 +0800

    cloud2020-commit

--- cloud-config-client-3355/src/main/java/com/ysq/springcloud/controller/ConfigClientController.java
+++ cloud-config-client-3355/src/main/java/com/ysq/springcloud/controller/ConfigClientController.java
@@ -1,6 +1,7 @@
 package com.ysq.springcloud.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2022/10/27  17:12
  */
 @RestController
+@RefreshScope
 public class ConfigClientController {
     @Value("${config.info}")
     private String configInfo;
