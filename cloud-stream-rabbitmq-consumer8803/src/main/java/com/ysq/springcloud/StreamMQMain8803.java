e6226e7159e44bb9d70a4884ff7e846ed5a7cb72
yansiqing <2457008948@qq.com>
2022-11-01 17:09:33 +0800

    cloud2020-commit

---
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/com/ysq/springcloud/StreamMQMain8803.java
@@ -0,0 +1,17 @@
+package com.ysq.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 功能描述
+ *
+ * @author 闫思晴
+ * @date 2022/11/01  16:43
+ */
+@SpringBootApplication
+public class StreamMQMain8803 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQMain8803.class,args);
+    }
+}
